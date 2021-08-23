e7f409ee771d95d1b0ae0fa58c053c2321e7258c
davidberiro <deebenchimol@gmail.com>
2021-08-23 21:34:32 +0200

    updates

--- contracts/Presale.sol
+++ contracts/Presale.sol
@@ -1,27 +1,35 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.6.12;
 
-import "@pancakeswap/pancake-swap-lib/contracts/token/BEP20/BEP20.sol";
-import "@pancakeswap/pancake-swap-lib/contracts/math/SafeMath.sol";
+import "./BEP20.sol";
 
 contract Presale is BEP20('PRESALE', 'PRESALE') {
     using SafeMath for uint;
 
     uint maxBNBPerAddress;
     uint tokenPerBNB;
+    uint BNBCap;
 
-    bool isActive;
+    uint totalBNBReceived = 0;
+    bool isActive = false;
 
     mapping (address => bool) public isWhitelisted;
     mapping (address => uint) public totalBNBSent;
 
-    constructor(uint _maxBNBPerAddress, uint _tokenPerBNB) public {
+    constructor(uint _maxBNBPerAddress, uint _tokenPerBNB, uint _BNBCap) public {
         maxBNBPerAddress = _maxBNBPerAddress;
         tokenPerBNB = _tokenPerBNB;
+        BNBCap = _BNBCap;
     }
 
     function setWhitelist(address _user) public onlyOwner {
         isWhitelisted[_user] = true;
+    }
+
+    function setWhitelists(address[] memory _users) public onlyOwner {
+        for (uint i = 0; i < _users.length; i++) {
+            isWhitelisted[_users[i]] = true;
+        }
     }
 
     function setActive(bool _active) public onlyOwner {
@@ -35,9 +43,23 @@ contract Presale is BEP20('PRESALE', 'PRESALE') {
 	receive() external payable {
         require(isActive, "Presale not active");
 		require(isWhitelisted[msg.sender], "Sender not whitelisted");
-        require(totalBNBSent[msg.sender].add(msg.value) <= maxBNBPerAddress, "Maximum BNB limit reached");
+        require(totalBNBSent[msg.sender].add(msg.value) <= maxBNBPerAddress, "Maximum BNB user limit reached");
+        require(totalBNBReceived.add(msg.value) <= BNBCap, "Maximum BNB cap reached");
         totalBNBSent[msg.sender] = totalBNBSent[msg.sender].add(msg.value);
+        BNBCap = BNBCap.add(msg.value);
         uint amount = (msg.value).mul(tokenPerBNB).div(1e18);
         _mint(msg.sender, amount);
 	}
+
+    function transfer(address recipient, uint256 amount) public override returns (bool) {
+        require(false, "Cannot transfer");
+    }
+
+    function transferFrom(
+        address sender,
+        address recipient,
+        uint256 amount
+    ) public override returns (bool) {
+        require(false, "Cannot transfer from");
+    }
 }
